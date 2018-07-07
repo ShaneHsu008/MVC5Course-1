b691c77665322976f07b302446a46a4312be7c66
ShaneHsu <shane@miniasp.com>
2018-07-07 11:28:39 +0800

    #05替Product.Price實作顯示格式設定（[DisplayFormat（DataFormatString =“{0：N ...
    ... 0}“）]）

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -31,6 +31,7 @@ namespace MVC5Course.Models
         public Nullable<decimal> Price { get; set; }
 
         [Required]
+        [DisplayFormat(DataFormatString="{0：N0}")]
         public Nullable<bool> Active { get; set; }
 
         [Required]
