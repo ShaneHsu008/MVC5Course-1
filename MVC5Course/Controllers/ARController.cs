ef2f04a2b49894a63aa078d7469261cb40d000d9
ShaneHsu <shane@miniasp.com>
2018-07-08 16:14:28 +0800

    #20 將字串當成 Model 傳到 View 並顯示 Model 內容

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewTest()
+        {
+            string model = "My Data";
+            return View((object)model);
+        }
+    }
+}
